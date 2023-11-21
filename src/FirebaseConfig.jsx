b3f57f4f0f76418dc6e11fa9431a7dfe7502d20f
Saimum-14028 <md.amiralfahim@gmail.com>
2023-11-21 22:29:58 +0600

    Basic Setup Completed Alhamdulillah

---
+++ src/FirebaseConfig.jsx
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyD-GtOsddTk17CEI2ZS--d_iDrw1quNndU",
+  authDomain: "my-education-event-management.firebaseapp.com",
+  projectId: "my-education-event-management",
+  storageBucket: "my-education-event-management.appspot.com",
+  messagingSenderId: "40189792962",
+  appId: "1:40189792962:web:7d2944f81d3d374a8c615b"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
---
+++ src/Route.jsx
@@ -0,0 +1,40 @@
+import { createBrowserRouter } from "react-router-dom";
+import MainLayout from "./MainLayout";
+
+const myCreatedRoute =  createBrowserRouter([
+    {
+
+        path : "/",
+        element : <MainLayout></MainLayout>,
+        errorElement: <ErrorPage></ErrorPage>,
+        children : [
+            {
+                path : "/",
+                element : <Home></Home>,
+            },
+            {
+                path : "/my events",
+                element : <PrivateRoute><MyEvents></MyEvents></PrivateRoute>
+            },
+            {
+                path : "/donation",
+                element : <PrivateRoute><Donation></Donation></PrivateRoute>
+            },
+            {
+                path : "service/:id",
+                element : <PrivateRoute><ServiceDetails></ServiceDetails></PrivateRoute>
+            },
+            {
+                path: "login",
+                element : <Login></Login>
+            },
+            {
+            path: "register",
+            element : <Register></Register>
+            },
+        ]
+
+    }
+])
+
+export default myCreatedRoute;
