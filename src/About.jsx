4a95e00c3eb6450e09ac6346c8a19b535ce12dd0
Saimum-14028 <md.amiralfahim@gmail.com>
2023-11-22 13:17:23 +0600

    Home  page completed  Alhamdulillah

--- src/About.jsx
+++ src/About.jsx
@@ -2,8 +2,9 @@ import React from 'react';
 
 const About = () => {
     return (
-        <div>
-            <h1>About Us</h1>
+        <div className='w-11/12 mx-auto'>
+            <h1 className="mb-10 text-5xl font-bold text-black text-center">About Us</h1>
+            <p className='text-lg font-semibold text-justify'>We know that Event management is crucial for ensuring the seamless execution of gatherings, conferences, and celebrations. It brings structure to chaos, emphasizing meticulous planning, coordination, and execution to create memorable and impactful experiences. Event management is vital for optimizing resources, enhancing participant engagement, and achieving the desired objectives of an event. It involves strategic decision-making, attention to detail, and effective communication to deliver events that leave a lasting impression.</p>
         </div>
     );
 };
--- src/Services.jsx
+++ src/Services.jsx
@@ -4,6 +4,7 @@ import { Link } from 'react-router-dom';
 const Services = ({cards}) => {
     return (
         <div className="w-11/12 mx-auto">
+            <h1 className="text-5xl font-bold text-black text-center">Our Services</h1>
             <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 py-10'>
                 {
                     cards?.map(card=><div key={card.id}>
